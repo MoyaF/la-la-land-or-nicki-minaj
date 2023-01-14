f13e9dd549e1f9a111418f0b75860fe36e9d1000
MoyaFaustoPoR <f.moya@portofrotterdam.com>
2023-01-14 14:57:45 +0100

    feature: add ktlint

--- src/main/kotlin/io/moya/lalalandornickyminaj/LaLaLandOrNickyMinajApplication.kt
+++ src/main/kotlin/io/moya/lalalandornickyminaj/LaLaLandOrNickyMinajApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class LaLaLandOrNickyMinajApplication
 
 fun main(args: Array<String>) {
-	runApplication<LaLaLandOrNickyMinajApplication>(*args)
+    runApplication<LaLaLandOrNickyMinajApplication>(*args)
 }
