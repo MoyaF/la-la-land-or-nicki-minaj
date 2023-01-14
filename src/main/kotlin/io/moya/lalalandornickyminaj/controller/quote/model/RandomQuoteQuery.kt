ad19eb4bc7005ebac1510d595bea3a5b0518071c
MoyaFaustoPoR <f.moya@portofrotterdam.com>
2023-01-14 19:22:14 +0100

    fix: remove snake case strategy

--- src/main/kotlin/io/moya/lalalandornickyminaj/controller/quote/model/RandomQuoteQuery.kt
+++ src/main/kotlin/io/moya/lalalandornickyminaj/controller/quote/model/RandomQuoteQuery.kt
@@ -1,10 +1,7 @@
 package io.moya.lalalandornickyminaj.controller.quote.model
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy
-import com.fasterxml.jackson.databind.annotation.JsonNaming
 import java.util.UUID
 
-@JsonNaming(value = SnakeCaseStrategy::class)
 data class RandomQuoteQuery(
     val id: UUID,
     val text: String,
